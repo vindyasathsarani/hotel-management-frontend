d18e8277438ac4b54c69e3f02bde9a404edb158e
Vindya Sathsarani <123439572+vindyasathsarani@users.noreply.github.com>
2025-03-03 23:33:22 +0530

    Add logout functionality to clear token and reset user state

--- src/components/userData/userdata.jsx
+++ src/components/userData/userdata.jsx
@@ -5,12 +5,14 @@ import { useState, useEffect } from "react";
 function UserTag(props) {
 
   const [name, setName] = useState("")
+  const [userFound, setUserFound] = useState(false)
+  
   const token = localStorage.getItem("token")
-
 
 
   useEffect(()=>{
     if(token != null){
+
       axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/",{
         headers : {
           Authorization : "Bearer " + token,
@@ -20,16 +22,26 @@ function UserTag(props) {
         console.log(res);
         
         setName(res.data.user.firstName + " " + res.data.user.lastName);
+        setUserFound(true)
         
       })
       
+    }else{
+      setName("")
     }
-  },[]);
+  },[userFound]);
 
   return (
     <div className="absolute right-0 flex items-center cursor-pointer mr-2">
       <img className="rounded-full w-[50px] h-[50px]" src={props.imageLink} alt="User" />
       <span className="text-white ml-[5px] text-xl">{name}</span>
+      <button onClick={()=>{
+        localStorage.removeItem("token")
+        setUserFound(false)
+        
+      }}>
+      Logout
+      </button>
     </div>
   );
 }
