a5c6e55b35f4354d2b11608780a69432ca4d66e9
Vindya Sathsarani <123439572+vindyasathsarani@users.noreply.github.com>
2025-03-16 01:45:16 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures the base path is set correctly for routing
 })
