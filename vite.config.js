eee03e074b3b123e9259fbf048e8f3d6e8d0a99f
Vindya Sathsarani <123439572+vindyasathsarani@users.noreply.github.com>
2025-03-16 02:49:14 +0530

    netlify file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ensures the base path is set correctly for routing
-})
+ import react from '@vitejs/plugin-react'
+ 
+ // https://vitejs.dev/config/
+ export default defineConfig({
+   plugins: [react()],
+ })
